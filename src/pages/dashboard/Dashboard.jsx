1b21c6346ffe8cb21d17653210193f27c33acc93
prachi-sahu-codes <prachisahu998@gmail.com>
2023-10-03 20:56:56 +0530

    feat: icons added

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -6,11 +6,12 @@ import {
   calcTotalCaloriesConsumed,
   calcGoalCalories,
 } from "../../store/activityStore/action";
+import { FaFire, FaUtensils, FaCheck, FaBullseye, FaHourglassHalf } from "react-icons/fa";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
- 
+
   useEffect(() => {
     dispatch(calcTotalCaloriesBurned(state.auth.loggedUser?._id));
     dispatch(calcTotalCaloriesConsumed(state.auth.loggedUser?._id));
@@ -22,26 +23,27 @@ const Dashboard = () => {
       <div className="flex gap-5">
         <div className="w-72 text-mediumGray bg-bgBox border-2 border-iconPurple border-opacity-20 p-4 rounded-lg">
           <h2 className="text-blue">Total Calories Burned</h2>
-          <p className="text-white text-xl my-4">
+          <p className="flex items-center gap-2 text-white text-xl my-4">
+            <FaFire className="fill-red"/>
             {state?.activity?.totalCaloriesBurned}
           </p>
         </div>
         <div className="w-72 text-mediumGray bg-bgBox border-2 border-iconPurple border-opacity-20 p-4 rounded-lg">
           <h2 className="text-blue">Total Calories Consumed</h2>
-          <p className="text-white text-xl my-4">
-            {state?.activity?.totalCaloriesConsumed}
+          <p className="flex items-center gap-2 text-white text-xl my-4">
+           <FaUtensils className="fill-orange"/> {state?.activity?.totalCaloriesConsumed}
           </p>
         </div>
         <div className="w-72 text-mediumGray bg-bgBox border-2 border-iconPurple border-opacity-20 p-4 rounded-lg">
           <h2 className="text-blue">Total Calories Goal</h2>
-          <p className="text-white text-xl my-4">
-            {state?.activity?.totalCaloriesGoals}
+          <p className="flex items-center gap-2 text-white text-xl my-4">
+           <FaBullseye className="fill-green"/>{state?.activity?.totalCaloriesGoals}
           </p>
         </div>
         <div className="w-72 text-mediumGray bg-bgBox border-2 border-iconPurple border-opacity-20 p-4 rounded-lg">
           <h2 className="text-blue">Remaining Calories to Goal</h2>
-          <p className="text-white text-xl my-4">
-            {state?.activity?.totalCaloriesRemaining}
+          <p className="flex items-center gap-2 text-white text-xl my-4">
+            <FaHourglassHalf className="fill-red"/>{state?.activity?.totalCaloriesRemaining}
           </p>
         </div>
       </div>
