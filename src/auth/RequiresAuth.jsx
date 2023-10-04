fc0137ac1cb89d9d1c0e486209ae0f4ef01a076b
prachi-sahu-codes <prachisahu998@gmail.com>
2023-10-04 22:33:31 +0530

    refactor: token

--- src/auth/RequiresAuth.jsx
+++ src/auth/RequiresAuth.jsx
@@ -3,10 +3,11 @@ import { Navigate, useLocation } from "react-router-dom";
 import store from "../configureStore";
 import SideBar from "../components/sideBar/SideBar";
 import Footer from "../layout/footer/Footer";
+import { useSelector } from "react-redux";
 
 export const RequiresAuth = ({ children }) => {
   const location = useLocation();
-  const token = store.getState()?.auth?.token;
+  const token = useSelector((state) => state?.auth?.token);
   return token ? (
     <div>
       <div className="flex">
